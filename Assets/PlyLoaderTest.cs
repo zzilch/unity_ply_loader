ba3f666bc2b8dcc8d9a50b9ed3c6c8163ca62bc3
zzilch <vcchzy@gmail.com>
2019-05-09 17:04:13 +0800

    Remove 64k limit

--- Assets/PlyLoaderTest.cs
+++ Assets/PlyLoaderTest.cs
@@ -6,16 +6,13 @@ public class PlyLoaderTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
     PlyLoader loader = new PlyLoader();
-    //Mesh [] mesh = loader.load("2PTC_EI_bs_1.ply");
-    Mesh [] mesh = loader.load("cube.ply");
+    //Mesh mesh = loader.load("2PTC_EI_bs_1.ply");
+    Mesh mesh = loader.load("cube.ply");
 
-    for (int i = 0; i != mesh.Length; ++i) {
-      GameObject g = new GameObject();
-      mesh[i].name = g.name = "mesh" + i;
-      MeshFilter mf = g.AddComponent<MeshFilter>();
-      mf.mesh = mesh[i];
-      MeshRenderer mr = g.AddComponent<MeshRenderer>();
-    }
+    GameObject g = new GameObject();
+    MeshFilter mf = g.AddComponent<MeshFilter>();
+    mf.mesh = mesh;
+    MeshRenderer mr = g.AddComponent<MeshRenderer>();
   }
 	
 	// Update is called once per frame
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # unity_ply_loader
-Loader for Stanford Ply files
+Loader for Stanford Ply files  
+- **Remove 64k limit**
